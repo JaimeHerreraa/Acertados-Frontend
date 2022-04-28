27e22f0baf1bffd97f112483697341ef6cdc93b1
Jaime Herrera <jaimeherrera1224@gmail.com>
2022-04-28 14:22:47 -0500

    building the content component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Login from '../login/Login';
 import Header from '../header/Header';
+import Content from '../content/Content';
 
 function App() {
   return (
     <div className="app">
         <Header/>
+        <Content/>
     </div>
   );
 }
---
+++ src/components/content/Content.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import './Content.css';
+
+function Content() {
+    const [selectedValue, setSelectedValue] = useState()
+    const onValueChange = (event) => {
+        setSelectedValue(event.target.value);
+    }
+    return (
+        <div className={'questions-container'}>
+            <div className={'question'}>
+                <p className={'description'}>Javascript es un lenguaje: </p>
+                <form className={'options'}>
+
+                    <div className={'choice-group'}>
+                        <input type={'radio'} id={'choice-one'} value={'Compilado'}  checked={selectedValue === 'Compilado'} onChange={onValueChange}/>
+                        <label htmlFor={'choice-one'}>Compilado</label>
+                    </div>
+
+                    <div className={'choice-group'}>
+                        <input type={'radio'} id={'choice-two'} value={'Interpretado'}  checked={selectedValue === 'Interpretado'} onChange={onValueChange}/>
+                        <label htmlFor={'choice-two'}>Interpretado</label>
+                    </div>
+
+                    <div className={'choice-group'}>
+                        <input type={'radio'} id={'choice-three'}value={'Hibrido'}  checked={selectedValue === 'Hibrido'} onChange={onValueChange}/>
+                        <label htmlFor={'choice-three'}>Hibrido</label>
+                    </div>
+
+                    <div className={'choice-group'}>
+                        <input type={'radio'} id={'choice-four'} value={'No Compilado'} checked={selectedValue === 'No Compilado'} onChange={onValueChange}/>
+                        <label htmlFor={'choice-four'}>No compilado</label>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Content;
